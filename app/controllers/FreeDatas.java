0c612ba6bd2bfdc0bbebf0271ac4fd6778c95f0d
fsznajderman <fabszn@gmail.com>
2012-06-09 16:32:26 +0200

    add new controler to expose statistics

--- app/controllers/FreeDatas.java
+++ app/controllers/FreeDatas.java
@@ -1,9 +1,6 @@
 package controllers;
 
-import models.ViewInformations;
-import models.domain.Article;
 import models.domain.Statistiques;
-import models.domain.Tag;
 import play.mvc.Controller;
 
 import java.util.List;
@@ -20,12 +17,10 @@ public class FreeDatas extends Controller {
     public static void freeStatGplus() {
 
 
+        final List<Statistiques> statistiques = Statistiques.q().filter("current", true).asList();
 
 
-        final List<Statistiques> statistiques = Statistiques.q().filter("current",true).asList();
-
-
-       Statistiques stats = statistiques.get(statistiques.size()-1);
+        Statistiques stats = statistiques.get(statistiques.size() - 1);
 
 
         renderJSON(stats);
