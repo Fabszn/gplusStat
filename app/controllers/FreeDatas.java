0082a71c83a624f919ecf2418cbb7f40f2b849aa
fsznajderman <fabszn@gmail.com>
2013-05-27 00:05:58 +0200

    add android datas

--- app/controllers/FreeDatas.java
+++ app/controllers/FreeDatas.java
@@ -1,6 +1,8 @@
 package controllers;
 
 
+import com.google.common.base.Function;
+import com.google.common.collect.Collections2;
 import com.google.common.collect.Lists;
 import models.ViewInformations;
 import models.domain.Article;
@@ -8,6 +10,8 @@ import models.domain.Statistiques;
 import models.domain.Tag;
 import play.mvc.Controller;
 
+import javax.annotation.Nullable;
+import java.util.Collection;
 import java.util.List;
 
 
@@ -31,9 +35,25 @@ public class FreeDatas extends Controller {
 
 
         Statistiques stats = statistiques.get(statistiques.size() - 1);
-          stats.setTitleMatrix(viewInformations.getTitleMatrix());
+        stats.setTitleMatrix(viewInformations.getTitleMatrix());
 
         renderJSON(stats);
+    }
+
+    public static void titles() {
+
+        final List<Article> articles = Article.q().filter("current", true).asList();
+
+
+        Collection<String> titles = Collections2.transform(articles, new Function<Article, String>() {
+            public String apply(@Nullable Article article) {
+                return article.getTitle();
+            }
+        });
+
+
+
+        renderJSON(titles);
     }
 
 }
